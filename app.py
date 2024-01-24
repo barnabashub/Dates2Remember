33663f07ea8c8c3f4f2352d01f05cde4669cd92b
toth.barnabas <toth.barnabas@kozut.hu>
2024-01-24 16:59:53 +0100

    reboot to postgre
    Co-authored-by: barnabashub <66264092+barnabashub@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, jsonify, render_template, request, send_from_directory
 from flask_wtf.csrf import CSRFProtect
-import boto3
+import psycopg2
 from datetime import datetime
 import uuid
 import os
 import yaml
 
-# Read the config file
+# config to postgres
 try:
-    with open('config/config_basic.yml', 'r') as file:
+    with open('config/conf_pgre_conn.yml', 'r') as file:
         config = yaml.safe_load(file)
 except FileNotFoundError as e:
     print(e)
@@ -17,19 +17,23 @@ app = Flask(__name__)
 csrf = CSRFProtect(app)
 app.config['SECRET_KEY'] = config['appconfigsecretkey']
 
-# AWS DynamoDB configurations
-dynamodb = boto3.resource(
-                        config['db'],
-                        region_name = config['region'], 
-                        aws_access_key_id = config['keyid'],
-                        aws_secret_access_key = config['key'])
+try:
+    db_host = config['host']
+    db_port = config['port']
+    db_user = config['user']
+    db_password = config['password']
+    db_name = config['database']
 
-table = dynamodb.Table(config['datestable'])
-print("log: ", [table.name for table in dynamodb.tables.all()])
-userids = dynamodb.Table('users')
-# Check if 'userids' table exists
-if 'userids' not in [table.name for table in dynamodb.tables.all()]:
-    print("Error: 'userids' table does not exist.")
+    connection = psycopg2.connect(
+        host=db_host,
+        port=db_port,
+        user=db_user,
+        password=db_password,
+        database=db_name
+    )
+    cursor = connection.cursor()
+except Exception as e:
+    print(f"Error: {e}")
 
 # Welcome page
 @app.route('/')
@@ -39,10 +43,9 @@ def welcome():
 # User page with id
 @app.route('/d/<user_id>')
 def user_page(user_id):
-    print("errorlog: ", str(user_id))
-    if "Item" in userids.get_item(Key={'userID': str(user_id)}):
+    cursor.execute(f'select user_id from users where user_id = {user_id}')
+    if len(cursor.fetchall()) == 1:
         return render_template('d.html')
-        
     else:
         return render_template('404.html')
 
@@ -54,30 +57,8 @@ def call_create_new_page():
 #Post new user id
 @app.route('/create_new', methods=['POST'])
 def save_data():
-    print("function called")
-    # Function checks if userid is reserved
-    def is_uuid_reserved(check_uuid):
-        print("next to check id")
-        response = userids.get_item(Key={'userID': str(check_uuid)})
-        print("id check is ready")
-        return 'Item' in response
-    
-    newID = str(uuid.uuid4())
-    if (is_uuid_reserved(newID)):
-        while is_uuid_reserved(newID):
-            newID = str(uuid.uuid4())
-
-    print("new id is ready")
-
     try:
-        new_item = {
-            'userID': newID,
-            'name': request.form.get('dataName'),
-            'date': str(datetime.now())
-        }
-        print("next is put item")
-        userids.put_item(Item=new_item)
-        print("put item is ready")
+        cursor.execute(f"insert into users (username, creationdate) values ('{request.form.get('dataName')}', '{str(datetime.now())}')")
         return jsonify({'message': 'Data saved successfully'})
     except Exception as e:
         return {'error': str(e)}
