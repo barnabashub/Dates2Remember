b02a5bcfdcbca5f51e50cd8f755f882a28c115b2
toth.barnabas <toth.barnabas@kozut.hu>
2024-07-16 11:35:29 +0200

    dateshark update

--- DatesHark.py
+++ DatesHark.py
@@ -30,3 +30,24 @@ class DatesHark:
             return True
         else:
             return False
+        
+    @staticmethod
+    def get_next_dates(date, quantity):
+        found_dates = []
+        while len(found_dates) < quantity:
+            date += 1
+            if DatesHark.any_jubilee(date, datetime.now()):
+                found_dates.append(date)
+
+    @staticmethod
+    def get_howold_indays(date1, date2):
+        dt1 = datetime.strptime(date1["date"], "%Y-%m-%d")
+        dt2 = date2
+        delta = abs((dt2 - dt1).days)
+        return delta
+
+    @staticmethod
+    def get_howold_inmonths(date1, date2):
+        dt1 = datetime.strptime(date1["date"], "%Y-%m-%d")
+        dt2 = date2
+        return (dt2.year - dt1.year) * 12 + dt2.month - dt1.month
